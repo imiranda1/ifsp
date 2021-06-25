615136480024c385c4448d4234cf7302cfd44d74
Icaro Miranda <icaroaugustomiranda@gmail.com>
2021-06-24 22:09:41 -0300

    layout calculadora

---
+++ Calculadora/app/src/main/java/br/com/imiranda/calculadora/MainActivity.java
@@ -0,0 +1,15 @@
+package br.com.imiranda.calculadora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide(); //esconder barra de ações
+    }
+}
