4ab9eea2405b8d1dd919cdd31c8e1e1d3d566c31
Icaro Miranda <icaroaugustomiranda@gmail.com>
2021-06-29 20:00:05 -0300

    exercício proposto

--- Dados/app/src/main/java/br/com/imiranda/dados/MainActivity.kt
+++ Dados/app/src/main/java/br/com/imiranda/dados/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import br.com.imiranda.dados.databinding.ActivityMainBinding
@@ -13,47 +14,70 @@ import kotlin.random.nextInt
 
 class MainActivity : AppCompatActivity() {
     private lateinit var activityMainBinding: ActivityMainBinding
-    private lateinit var gerardorRamdomico: Random
+    private lateinit var geradorRandomico: Random
+
     private lateinit var settingsActivityLauncher: ActivityResultLauncher<Intent>
+    private var numberOfDices: Int = 1
+    private var numberOfFaces: Int = 6
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
 
+        geradorRandomico = Random(System.currentTimeMillis())
 
-        gerardorRamdomico = Random(System.currentTimeMillis())
+        activityMainBinding.jogarDadoBt.setOnClickListener {
 
-        activityMainBinding.jogarDadoBt.setOnClickListener{
-            val resultado: Int = gerardorRamdomico.nextInt(1..6)
-            "A face sorteada foi $resultado".also{ activityMainBinding.resultadoTv.text = it}
-            val nomeImagem = "dice_${resultado}"
+            val firstResult: Int = geradorRandomico.nextInt(1..numberOfFaces)
+            val msg = "A(s) face(s) sorteada(s) foi(ram) $firstResult".also {
+                activityMainBinding.resultadoTv.text = it
+            }
+            val nomePrimeiraImagem = "dice_$firstResult"
+
             activityMainBinding.resultadoIv.setImageResource(
-                resources.getIdentifier(nomeImagem,"mipmap",packageName)
+                resources.getIdentifier(nomePrimeiraImagem, "mipmap", packageName)
             )
 
+            if (numberOfDices == 2) {
+                val secondResult: Int = geradorRandomico.nextInt(1..numberOfFaces)
+                "$msg e $secondResult".also { activityMainBinding.resultadoTv.text = it }
+                val nomeSegundaImagem = "dice_$secondResult"
+
+                activityMainBinding.resultado2Iv.setImageResource(
+                    resources.getIdentifier(nomeSegundaImagem, "mipmap", packageName)
+                )
+                if (numberOfFaces <= 6) {
+                    activityMainBinding.resultado2Iv.visibility = View.VISIBLE
+                } else {
+                    activityMainBinding.resultadoIv.visibility = View.GONE
+                    activityMainBinding.resultado2Iv.visibility = View.GONE
+                }
+            } else if (numberOfFaces > 6) {
+                activityMainBinding.resultadoIv.visibility = View.GONE
+                activityMainBinding.resultado2Iv.visibility = View.GONE
+            } else {
+                activityMainBinding.resultado2Iv.visibility = View.GONE
+            }
         }
-
-        settingsActivityLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
-            if(result.resultCode == RESULT_OK){
-                //modificações na view
-                if(result.data != null){
-                   val configuracao:Configuracao? =  result.data?.getParcelableExtra<Configuracao>(Intent.EXTRA_USER)
-                    //Exercicio.. Modificação da view.
-
+        settingsActivityLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                if (result.data != null) {
+                    val configuracao: Configuracao? = result.data?.getParcelableExtra<Configuracao>(Intent.EXTRA_USER)
+                    numberOfDices = configuracao!!.numeroDado
+                    numberOfFaces = configuracao!!.numeroFaces
                 }
             }
         }
     }
 
-
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main_menu,menu)
-        return super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(item.itemId == R.id.settingsMi){
+        if (item.itemId == R.id.settingsMi) {
             val settingsIntent = Intent(this, SettingsActivity::class.java)
             settingsActivityLauncher.launch(settingsIntent)
             return true
